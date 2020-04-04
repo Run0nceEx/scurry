8078862af7294c6f86fe87035698146f899dd876
Skarlett@github.com <Skarlett@github.com>
2020-04-04 07:41:50 -0500

    Cleaned up build files from compat layer

--- px-compat/target/debug/deps/px_db-0ab528d7015f2a5e.d
+++
@@ -1,8 +0,0 @@
-/home/ghost/projects/px-engine/libs/px-compat/target/debug/deps/px_db-0ab528d7015f2a5e.rmeta: src/lib.rs src/layer/mod.rs src/layer/database.rs src/layer/orchestrator.rs
-
-/home/ghost/projects/px-engine/libs/px-compat/target/debug/deps/px_db-0ab528d7015f2a5e.d: src/lib.rs src/layer/mod.rs src/layer/database.rs src/layer/orchestrator.rs
-
-src/lib.rs:
-src/layer/mod.rs:
-src/layer/database.rs:
-src/layer/orchestrator.rs:
