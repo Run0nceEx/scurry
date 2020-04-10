ce42ccd69d30d76adcf2d6f3ce627632a7a51942
Skarlett <zykl@protonmail.com>
2020-04-10 08:52:25 -0500

    Added scheduler framework
    Still deciding on how to pass around values inside of async threads
    (independent coroutines)
    
    Currently a consumer just waits for data from jobs, and more jobs
    
    Changes to be committed:
    	modified:   Cargo.toml
    	modified:   src/scheduler/manager.rs

--- pxcore/src/scheduler/manager.rs
+++ pxcore/src/scheduler/manager.rs
@@ -1,37 +1,122 @@
-use std::future::Future;
-use std::time::Instant;
+use std::{
+    time::{Instant, Duration},
+    collections::HashMap,
+};
+use tokio::sync::mpsc;
+use async_trait::async_trait;
 
-trait Task<Arg: Sized, R>: Fn(Arg) -> R + 'static
-where R: Future<Output=R> {}
 
-
-struct Job<Arg, R> {
-    job: Box<dyn Task<Arg, R>>,
-    queued_at: Instant,
+#[derive(Copy, Clone)]
+struct JobMeta {
+    id: uuid::Uuid,
+    exec_at: Instant,
     created_at: Instant
 }
 
-
-struct ProxyMine(Job<(), ()>);
-struct ProxyDiscover(Job<(), ()>);
-
-
-trait IntoJob<Arg, R> {
-    fn into_task(self) -> Box<Task<Arg, R>>;
-    fn as_job<'a>(&'a self) -> &'a Job<Arg, R>;
+impl std::cmp::PartialEq for JobMeta {
+    fn eq(&self, other: &Self) -> bool {
+        self.id == other.id
+    }
 }
 
 
-struct JobSchedule<Arg, R> {
-    tasks: Vec<Box<IntoJob<Arg, R>>>
+#[async_trait]
+trait Task<R>: Sized {
+    // Run task, and reproduce more jobs if needed
+    async fn exec(self) -> (R, Vec<(Self, JobMeta)>);
 }
 
 
-trait Scheduler {
-    fn emit_ready<Arg, R>(&mut self) -> Vec<Box<IntoJob<Arg, R>>>;
+struct Scheduler<T, R>
+{
+    val_consumer: mpsc::Sender<R>,
+    //job_consumer: mpsc::Sender<Vec<(T, JobMeta)>>,
+    jobs: HashMap<uuid::Uuid, T>,
+    schedule: HashMap<uuid::Uuid, JobMeta>
 }
 
-struct Manager<T: Scheduler> {
-    schedules: Vec<T>
+
+impl<T, R> Scheduler<T, R>
+where
+    T: Task<R> + Sync + Send + 'static,
+    R: Sync + Send + Copy + 'static
+{
+    fn run_tasks(&mut self, job_tx: mpsc::Sender<Vec<(T, JobMeta)>> ) {
+        let mut tasked = Vec::new(); 
+        for (id, meta) in self.schedule.iter() {
+            if Instant::now() - meta.exec_at > Duration::from_secs(0) {
+                tasked.push(id.clone());
+                
+                let mut vtx = self.val_consumer.clone();
+                let mut jtx = job_tx.clone();
+
+                match self.jobs.remove(&id) {
+                    Some(job) => {
+                        tokio::spawn(async move {
+                            // Fail handle?
+                            let (v, jobs) = job.exec().await;
+                            vtx.send(v).await;
+                            jtx.send(jobs).await;
+                        });
+
+                    }
+                    
+                    None => {} // Add error handle?
+                }
+            }
+        }
+
+        for id in tasked {
+            self.schedule.remove(&id);
+        }
+
+    }
 }
+
+
+struct A {
+
+}
+
+#[async_trait]
+impl Task<()> for A {
+    async fn exec(self) -> ((), Vec<(Self, JobMeta)>) {
+        (
+            (), vec![(
+                A {}, JobMeta {
+                    id: uuid::Uuid::default(),
+                    created_at: Instant::now(),
+                    exec_at: Instant::now()
+                }
+            )]
+        )
+    }
+}
+
+
+enum Schedules {
+    ExploreNet(Scheduler<A, ()>),
+
+}
+
+
+// struct Manager {
+//     schedules: Vec<>
+// }
+
+
+
+// trait GenericConsumer {
+    
+// }
+
+// trait GenericSchedule {
+//     fn emit_ready<Arg, R>(&mut self) -> Vec<JobSchedule<Arg, R>>;
+// }
+
+
+// struct Manager<T: GenericSchedule> {
+//     schedules: Vec<T>
+
+// }
 
