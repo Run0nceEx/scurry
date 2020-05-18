0b1ce811361ac793ef8dae76cdd2213b332ed1f6
Skarlett@github.com <Skarlett@github.com>
2020-05-18 11:54:26 -0500

    new restructure + core

--- pxcore/src/core/net/comm.rs
+++ pxcore/src/core/net/comm.rs
@@ -6,7 +6,7 @@ pub struct ErrorKind(String);
 
 
 #[async_trait::async_trait]
-pub trait Connection: Sized + 'static {
+pub trait Negotiate: Sized + 'static {
 
     async fn negotiate<T, C>(&self, addr: SocketAddr, protocol: T) -> Result<bool, ErrorKind>
     where 
@@ -16,17 +16,26 @@ pub trait Connection: Sized + 'static {
     {
         protocol.detect(C::init_connect(addr).await?).await
     }
+
+    async fn noop<C>(&self, addr: SocketAddr) -> Result<(), ErrorKind>
+    where
+        C: Connector<Self> + Send
+    {
+        C::init_connect(addr).await?;
+        Ok(())
+    }
 }
 
-impl<T> Connection for T 
+//Blanket expression for any type that has Connector<T> + Identifier<T>
+impl<T> Negotiate for T 
 where 
-    T: Connector<Self> + Identifier<Self> + Send + Sync + 'static {}
+    T: Connector<T> + Identifier<T> + Send + Sync + 'static {}
 
 
 #[async_trait::async_trait]
 /// Object used to represent the communication object spawned from `init_connect`
 /// and is used in `Indentifier`
-pub trait Connector<C: Connection> {
+pub trait Connector<C: Negotiate> {
     /// Constructor
     async fn init_connect(addr: SocketAddr) -> Result<C, ErrorKind>;
 }
@@ -34,17 +43,14 @@ pub trait Connector<C: Connection> {
 
 #[async_trait::async_trait]
 /// Identifies protocols
-pub trait Identifier<C: Connection + 'static>
+pub trait Identifier<C: Negotiate + 'static>
 {
     /// exec detection
     async fn detect(&self, con: C) -> Result<bool, ErrorKind>;
-
-    async fn noop_connect(&self, addr: SocketAddr) -> Result<(), ErrorKind> {
-        TcpStream::connect(addr).await?;
-        Ok(())
-    }
 }
 
+
+impl Negotiate for TcpStream {}
 
 #[async_trait::async_trait]
 impl Connector<TcpStream> for TcpStream {
@@ -53,16 +59,21 @@ impl Connector<TcpStream> for TcpStream {
     }
 }
 
+struct Socks5;
 
-impl Identifier<TcpStream> for 
+#[async_trait::async_trait]
+impl Identifier<TcpStream> for Socks5 {
+  async fn detect(&self, con: TcpStream) -> Result<bool, ErrorKind> {
+      Ok(true)
+  }
+}
 
-
-impl Connection for TcpStream {}
 
 struct A(TcpStream);
 impl A {
     async fn new(addr: SocketAddr) {
-        TcpStream::connect(addr).await;
+        let stream = TcpStream::connect(addr).await.unwrap();
+        Socks5{}.detect(stream);
     }
 }
 
