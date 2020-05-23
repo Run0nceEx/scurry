97c86c52c10a63efe6f458d3cfbdf63756299123
Skarlett <zykl@protonmail.com>
2020-05-23 02:48:32 -0500

    fixed imports

--- pxcore/corelib/src/features/mod.rs
+++ pxcore/corelib/src/features/mod.rs
@@ -2,3 +2,6 @@
 
 #[feature(lua)]
 pub mod pxlua;
+
+#[feature(default_scanners)]
+pub mod protocols;
--- pxcore/corelib/src/lib.rs
+++ pxcore/corelib/src/lib.rs
@@ -3,7 +3,6 @@ mod comm;
 #[macro_use] extern crate lazy_static;
 
 pub mod features;
-pub mod protocols;
 pub use comm::{Identifier, Connector, Negotiate, TcpStream};
 
 
