91c4d8326d9e3e3696748246c518141b13942b1c
Skarlett <zykl@protonmail.com>
2020-09-22 11:07:13 -0500

    removed files and added media by its self

--- engine/src/discovery/host/mod.rs
+++
@@ -1,2 +0,0 @@
-// contains code for discovering new hosts
-//mod synfan;
--- engine/src/state.rs
+++
@@ -1,9 +0,0 @@
-struct State {
-    display_timer: (Instant, f32),
-
-
-    connect_pool: CronPool<OpenPortJob, PortState, Job>,
-    job_stash: Stash<Job>,
-    job_buf: Vec<(CronMeta, SignalControl, Option<PortState>, Job)>,
-
-}
